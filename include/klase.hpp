f496db8fa8e9f443554a10aedc673d0ccf8d6f08
asmir98 <asmir.k17@gmail.com>
2022-02-11 22:24:24 +0100

    Succes

--- include/klase.hpp
+++ include/klase.hpp
@@ -1,5 +1,45 @@
-class column {
-	double count = 0;
-	double quantity = 0;
-	double price = 0;
+#include <iostream>
+
+class ColumnInTable {
+		
+		double count = 0;
+		
+		double quantity = 0;
+		
+		double price = 0;
+	
+	public:
+
+		virtual void print()=0;
+
+		//Getters
+		double getCount(){ return count; }
+		
+		double getQuantity(){ return quantity; }
+		
+		double getPrice(){ return price; }
+
+		//Setters
+		void setCount(double countValue){ count = countValue; }
+
+		void setQuantity(double quantityValue){ quantity = quantityValue; }
+
+		void setPrice(double priceValue){ price = priceValue; }
 };
+
+class BID : public ColumnInTable{
+	
+	public:
+		void print(){
+			std::cout << "Kupac zeli da kupi " << getCount() << " tura dionica u kolicinama od " << getQuantity() << " komada po cijeni " << getPrice() << ".";
+		}
+};
+
+class ASK : public ColumnInTable{
+	
+	public:
+		void print(){
+			std::cout << "Prodavac zeli da proda " << getCount() << " tura dionica u kolicinama od " << getQuantity() << " komada po cijeni " << getPrice() << ".";
+		}
+};
+
--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
-#include <boost/property_tree/ptree_fwd.hpp>
 #include <fstream>
 #include <iostream>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/reader.h>
+#include <jsoncpp/json/value.h>
+#include "include/klase.hpp"
 
 using namespace std;
-using namespace boost::property_tree;
+using namespace Json;
+
+Value parsiranjeJSONA(string jsonName){
+	ifstream loadJSON(jsonName);
+	Value realJSON;
+	Reader reader;
+	reader.parse(loadJSON,realJSON);
+	return realJSON;
+}
+
+BID konverzija(){
+	Value koristit = parsiranjeJSONA("pomocni.json");
+	BID zaVratit;
+	//double broj = (koristit["book"]["bid"][0]).asDouble();
+	zaVratit.setCount(koristit["book"]["bid"][0].asDouble());
+	zaVratit.setQuantity(koristit["book"]["bid"][1].asDouble());
+	zaVratit.setPrice(koristit["book"]["bid"][2].asDouble());
+	return zaVratit;
+}
 
 int main(){
-	ptree root;
-	read_json("pomocni.json",root);
-	string knjiga = root.get<string>("book.symbol");
-	cout << knjiga;
+	//ifstream fajl("pomocni.json");
+	//Json::Value fajlic;
+	//Json::Reader reader;
+	
+	//reader.parse(fajl, fajlic);
+	BID varijabla = konverzija();
+	varijabla.print();
+	//Value jsoncic = parsiranjeJSONA("pomocni.json");
+	//cout << "Total json data is: \n" << jsoncic << endl;
+	//cout << "Jedno ime samo je: " << jsoncic["book"]["bid"][0] << endl;
+	//cout << "Jedno ime od jednog imena je " << fajlic["book"]["ask"] << endl;
 	return 0;
 }
